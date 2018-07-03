da3d84867f6027cfd53cc2f368824a143e3f3250
Abdullah TÜREL <39696721+abdullahturel@users.noreply.github.com>
2018-07-03 20:22:06 +0300

    MVC
    Class dosyalarının çağrılma yöntemi değiştirildi.

--- System/init.php
+++ System/init.php
@@ -9,10 +9,17 @@ foreach (glob(__DIR__ . '/Core/*.php') as $coreFile){
 	require_once strtolower($coreFile);
 }
 
-// Application Classes Dosyalarını Yükler
+/* Application Classes Dosyalarını Yükler
 foreach (glob(PATH .'/'. strtolower(CLASSES_PATH) .'/*.php') as $className){
 	require_once strtolower($className);
+}*/
+
+//Application Classes Dosyalarını Class Çağrıldığında Yükler
+function appClasses($className)
+{
+    require PATH .'/'. strtolower(CLASSES_PATH) .'/'. strtolower($className) . '.php';
 }
+spl_autoload_register('appClasses');
 
 // Application Helper Dosyalarını Yükler
 foreach (glob(PATH .'/'. strtolower(HELPERS_PATH) .'/*.php') as $helperFile){
